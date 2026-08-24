dfab4317d4d1f6fbf823f719788e170eecdb8993
piyu5794 <piyu5794@gmail.com>
2026-08-24 16:04:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.67%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+
+        vector<string> ans;
+        int idx = 0;
+        for (int i = 1; i <= n && idx < target.size(); i++) {
+
+            ans.push_back("Push");
+
+            if (i == target[idx])
+                idx++;
+            else {
+                ans.push_back("Pop");
+            }
+        }
+        return ans;
+    }
+};
