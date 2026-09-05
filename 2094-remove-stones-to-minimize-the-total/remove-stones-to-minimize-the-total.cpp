9eca1bbdaa84465bb14ac4cd64d3e8ca0aeb00c5
piyu5794 <piyu5794@gmail.com>
2026-09-05 21:33:10 +0530

    Time: 293 ms (48.74%) | Memory: 108.7 MB (60.32%) - LeetSync

---
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        priority_queue<int> pq;
+
+        for (int x : piles) {
+            pq.push(x);
+        }
+
+        while (k--) {
+            int x = pq.top();
+            pq.pop();
+
+            x -= x / 2;
+            pq.push(x);
+        }
+
+        int ans = 0;
+
+        while (!pq.empty()) {
+            ans += pq.top();
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
