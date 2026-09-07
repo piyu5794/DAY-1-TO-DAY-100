ac246e2dbe5969795ad8c0c1ab33b3b96b162e21
piyu5794 <piyu5794@gmail.com>
2026-09-07 15:29:01 +0530

    Time: 1 ms (29.48%) | Memory: 9.5 MB (43.92%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> last(26,-1);
+        int n =s.size();
+        for(int i= 0; i<n; i++){
+            last[s[i] -'a'] =i;
+        }
+
+        stack<char> st;
+        vector<bool> used(26, false);
+        for(int i=0 ;i<n ;i++){
+            char c =s[i];
+            if(used[c -'a']) continue;
+            
+            while(!st.empty() && st.top() >c && last[st.top() -'a'] >i){
+            
+                used[st.top() -'a'] =false;
+                st.pop();
+            }
+            st.push(c);
+            used[c -'a'] =true;
+        }
+        string ans="";
+        while(!st.empty()){
+            ans =st.top() +ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
